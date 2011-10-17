d1ca64bf934771c92cef803a61503cb7d1159786
Sam Ritchie <sritchie09@gmail.com>
2011-10-17 12:44:58 -0700

    bumped dependency versions.

--- project.clj
+++ project.clj
@@ -1,15 +1,9 @@
-(defproject backtype/dfs-datastores-cascading "1.0.4"
+(defproject backtype/dfs-datastores-cascading "1.0.5"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :repositories {
-                 "conjars" "http://conjars.org/repo"
-                 }  
-
-  :dependencies [
-                 [backtype/dfs-datastores "1.0.3"]
-                 [cascading/cascading-core "1.2-wip-63" :exclusions [org.codehaus.janino/janino]]
-                 ]
-  :dev-dependencies [
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [junit/junit "3.8.2"]
-                    ])
+  :repositories {"conjars" "http://conjars.org/repo"}
+  :dependencies [[backtype/dfs-datastores "1.0.5"]
+                 [cascading/cascading-core "1.2.4"
+                  :exclusions [org.codehaus.janino/janino]]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [junit/junit "3.8.2"]])
