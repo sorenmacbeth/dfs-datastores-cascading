f901fa1b20d361e257e1d7a6a9e91d6570bcef03
Sam Ritchie <sritchie09@gmail.com>
2011-12-15 16:19:54 -0800

    Added note on proper versioning to README.

--- project.clj
+++ project.clj
@@ -6,10 +6,6 @@
   :dependencies [[backtype/dfs-datastores "1.0.5"]
                  [cascading/cascading-hadoop "2.0.0-wip-166"
                   :exclusions [org.codehaus.janino/janino
-                               org.apache.hadoop/hadoop-core
-                               thirdparty/jgrapht-jdk1.6
-                               riffle/riffle]]
-                 [thirdparty/jgrapht-jdk1.6 "0.8.1"]
-                 [riffle/riffle "0.1-dev"]]
+                               org.apache.hadoop/hadoop-core]]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [junit/junit "3.8.2"]])
