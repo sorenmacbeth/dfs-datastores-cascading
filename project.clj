6270fbaa07e7705476963c26d64db251f93e9374
Sam Ritchie <sritchie09@gmail.com>
2011-12-30 11:00:25 -0800

    Removed clojure testing harness.

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject backtype/dfs-datastores-cascading "1.1.0-SNAPSHOT"
-  :source-path "src/clj"
   :java-source-path "src/jvm"
   :java-test-path "test/jvm"
-  :test-path "test/clj"
   :javac-options {:debug "true" :fork "true"}
   :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[backtype/dfs-datastores "1.1.0-SNAPSHOT"]
@@ -10,5 +8,4 @@
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje "1.3.0"]
                      [junit/junit "3.8.2"]])
