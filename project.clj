b7ed9cd8a26724e5311ff6d7b299ca80b3025940
Sam Ritchie <sritchie09@gmail.com>
2012-01-11 19:53:18 -0500

    version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject backtype/dfs-datastores-cascading "1.1.1-SNAPSHOT"
+(defproject backtype/dfs-datastores-cascading "1.1.1"
   :java-source-path "src/jvm"
   :source-path "src/clj"
   :java-test-path "test/jvm"
