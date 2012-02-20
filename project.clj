6a27320e6120b45fc598c09d2b9b57f74fe45b65
Sam Ritchie <sritchie09@gmail.com>
2012-02-19 18:25:42 -0800

    Added travis CI.

--- project.clj
+++ project.clj
@@ -3,10 +3,15 @@
   :source-path "src/clj"
   :java-test-path "test/jvm"
   :javac-options {:debug "true" :fork "true"}
+  :javac-source-path [["src"] ["test"]]
+  :junit [["classes"]]
+  :junit-options {:fork "off" :haltonfailure "on"}
   :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[backtype/dfs-datastores "1.1.0"]
                  [cascading/cascading-hadoop "2.0.0-wip-184"
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [junit/junit "3.8.2"]])
+                     [lein-javac "1.3.0"]
+                     [lein-junit "1.0.0"]
+                     [junit "4.7"]])
